9b5775d31a04f995a8a78af0e5d592cd9ebae9c4
doziechuks <doziechuks1010@gmail.com>
2023-10-20 11:20:50 +0100

    built the navbar and the footer components

--- src/components/mainFooter/MainFooter.jsx
+++ src/components/mainFooter/MainFooter.jsx
@@ -1,7 +1,13 @@
 import styles from "./MainFooter.module.css";
 
 const MainFooter = () => {
-  return <footer className={styles.container}>footer</footer>;
+  return (
+    <footer className={styles.container}>
+      <p>
+        All rights reserved. Built by <span>Chuka</span> &copy; 2023
+      </p>
+    </footer>
+  );
 };
 
 export default MainFooter;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <nav className={styles.container}>nav bar</nav>;
+  return (
+    <nav className={styles.container}>
+      <Link to="/">
+        <h2>CHUKA.</h2>
+      </Link>
+      <div>ALT/SOE/023/0689</div>
+    </nav>
+  );
 };
 
 export default Navbar;
